a4e959488975d972106539f382cd965428b04e70
jay <jjnevis@gmail.com>
2011-03-09 20:25:21 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tph::Application.config.secret_token = 'fe314077692533ac371c46894989a8337ae9f48eee5caba7d05e6e1d1cb8b34a43ed7a54e3f3bd2c1bae3bfdaf611303a2a0c6f2461c3d18b958164b96520ca4'
