57af6ad6ed8aebdffbd96546b7c7a1971850f950
jay <jjnevis@gmail.com>
2011-03-10 23:34:28 +0000

    remove the email init file

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "thepineshouse.com",  
-  :user_name            => "jay",  
-  :password             => "jimbo111",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
-}
