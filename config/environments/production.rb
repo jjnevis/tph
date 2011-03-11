0a437dd10e49273d5458e9daf89e9c22283abd82
jay <jjnevis@gmail.com>
2011-03-11 00:46:25 +0000

    remove gmail smtp plugin and change prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,17 @@ Tph::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :tls            => true,
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'thepineshouse.com',
+    :authentication => :plain,
+    :user_name      => 'lucy@thepineshouse.com',
+    :password       => 'poppy1'
+  }
   
   # Enable threaded mode
   # config.threadsafe!
