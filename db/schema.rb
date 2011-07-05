0cd033e3cd60b42c274b8c8b04d4b99804475488
jay <jjnevis@gmail.com>
2011-07-05 12:26:15 +0100

    add page images engine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110616165749) do
+ActiveRecord::Schema.define(:version => 20110705111527) do
+
+  create_table "image_pages", :id => false, :force => true do |t|
+    t.integer "image_id"
+    t.integer "page_id"
+    t.integer "position"
+    t.text    "caption"
+  end
+
+  add_index "image_pages", ["image_id"], :name => "index_image_pages_on_image_id"
+  add_index "image_pages", ["page_id"], :name => "index_image_pages_on_page_id"
 
   create_table "images", :force => true do |t|
     t.string    "image_mime_type"
@@ -67,32 +77,32 @@ ActiveRecord::Schema.define(:version => 20110616165749) do
   add_index "page_parts", ["page_id"], :name => "index_page_parts_on_page_id"
 
   create_table "page_translations", :force => true do |t|
-    t.integer  "page_id"
-    t.string   "locale"
-    t.string   "title"
-    t.string   "custom_title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "page_id"
+    t.string    "locale"
+    t.string    "title"
+    t.string    "custom_title"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "page_translations", ["page_id"], :name => "index_page_translations_on_page_id"
 
   create_table "pages", :force => true do |t|
-    t.integer  "parent_id"
-    t.integer  "position"
-    t.string   "path"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "show_in_menu",        :default => true
-    t.string   "link_url"
-    t.string   "menu_match"
-    t.boolean  "deletable",           :default => true
-    t.string   "custom_title_type",   :default => "none"
-    t.boolean  "draft",               :default => false
-    t.boolean  "skip_to_first_child", :default => false
-    t.integer  "lft"
-    t.integer  "rgt"
-    t.integer  "depth"
+    t.integer   "parent_id"
+    t.integer   "position"
+    t.string    "path"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.boolean   "show_in_menu",        :default => true
+    t.string    "link_url"
+    t.string    "menu_match"
+    t.boolean   "deletable",           :default => true
+    t.string    "custom_title_type",   :default => "none"
+    t.boolean   "draft",               :default => false
+    t.boolean   "skip_to_first_child", :default => false
+    t.integer   "lft"
+    t.integer   "rgt"
+    t.integer   "depth"
   end
 
   add_index "pages", ["depth"], :name => "index_pages_on_depth"
@@ -138,13 +148,13 @@ ActiveRecord::Schema.define(:version => 20110616165749) do
   add_index "roles_users", ["user_id", "role_id"], :name => "index_roles_users_on_user_id_and_role_id"
 
   create_table "seo_meta", :force => true do |t|
-    t.integer  "seo_meta_id"
-    t.string   "seo_meta_type"
-    t.string   "browser_title"
-    t.string   "meta_keywords"
-    t.text     "meta_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "seo_meta_id"
+    t.string    "seo_meta_type"
+    t.string    "browser_title"
+    t.string    "meta_keywords"
+    t.text      "meta_description"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "seo_meta", ["id"], :name => "index_seo_meta_on_id"
@@ -174,21 +184,21 @@ ActiveRecord::Schema.define(:version => 20110616165749) do
   add_index "user_plugins", ["user_id", "name"], :name => "index_unique_user_plugins", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "username",             :null => false
-    t.string   "email",                :null => false
-    t.string   "encrypted_password",   :null => false
-    t.string   "persistence_token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "perishable_token"
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "sign_in_count"
-    t.string   "remember_token"
-    t.string   "reset_password_token"
-    t.datetime "remember_created_at"
+    t.string    "username",             :null => false
+    t.string    "email",                :null => false
+    t.string    "encrypted_password",   :null => false
+    t.string    "persistence_token"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "perishable_token"
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.integer   "sign_in_count"
+    t.string    "remember_token"
+    t.string    "reset_password_token"
+    t.timestamp "remember_created_at"
   end
 
   add_index "users", ["id"], :name => "index_users_on_id"
